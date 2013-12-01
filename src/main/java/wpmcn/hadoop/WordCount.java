8215e26169b15e59a3c8c1720accb350d3cb63b9
ibondarenko <ext-i.bondarenko@netbiscuits.com>
2013-12-01 17:46:51 +0200

    Fix for input/output path

--- src/main/java/wpmcn/hadoop/WordCount.java
+++ src/main/java/wpmcn/hadoop/WordCount.java
@@ -2,12 +2,15 @@ package wpmcn.hadoop;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -53,6 +56,8 @@ public class WordCount extends Configured implements Tool {
       job.setCombinerClass(WordCountReducer.class);
       job.setReducerClass(WordCountReducer.class);
 
+       FileInputFormat.addInputPath(job, new Path(args[0]));
+      FileOutputFormat.setOutputPath(job, new Path(args[1]));
       job.setInputFormatClass(TextInputFormat.class);
       job.setOutputFormatClass(TextOutputFormat.class);
 
